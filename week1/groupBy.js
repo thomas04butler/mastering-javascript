4575373a1e535cece8670aaeb0026606b22e2140
Thomas Butler <thomas04butler@gmail.com>
2026-04-11 12:47:28 +0200

    push groupBy

---
+++ week1/groupBy.js
@@ -0,0 +1,72 @@
+const peopleArray = [
+    {
+        firstname: 'Thomas',
+        country: 'Spain'
+    },
+    {
+        firstname: 'Godin',
+        country: 'Uruguay'
+    },
+        {
+        firstname: 'Butler',
+        country: 'Spain'
+    },
+    {
+        firstname: 'Federico',
+        country: 'Uruguay'
+    },
+        {
+        firstname: 'Lamine',
+        country: 'Morocco'
+    },
+    {
+        firstname: 'Joshua',
+        country: 'Germany'
+    },
+]
+
+function groupByCountry(people) {
+    let groupedPeopleArray = []
+
+    for (let i = 0; i < people.length; i++) {
+        const country = (people[i].country)
+        const firstname = (people[i].firstname)
+
+        let inArray = false
+
+        for (let f = 0; f < groupedPeopleArray.length; f++) {
+            if (groupedPeopleArray[f].label === country) {
+                inArray = true
+                groupedPeopleArray[f].children.push(firstname)
+
+                break
+            }
+        }
+        if (!inArray) {
+            groupedPeopleArray.push({label:country, 'children':[firstname]})
+        }
+    }
+    console.log(groupedPeopleArray)
+}
+
+function groupByCountry1(people) {
+    let sortedPeople = []
+
+    for (let i= 0; i < people.length; i++) {
+        country = people[i].country
+        firstname = people[i].firstname
+
+        let inArray = false
+
+        for (let j = 0; j <= sortedPeople.length; j++) {
+            if (country === sortedPeople[j]) {
+                inArray = true
+                sortedPeople[j].push(firstname)
+            }
+
+        }
+    }
+}
+
+groupByCountry1(peopleArray)
+//groupByCountry(peopleArray)
